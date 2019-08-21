7b5eb7d41f881035aa25b5bca372d50b692ceb83
pedroomedicina <pedromedina190@gmail.com>
2019-08-21 14:32:29 -0400

    added creating user-stock functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
-    has_many :users, through: :user_stocks
+  has_many :users, through: :user_stocks
 
 
     def self.find_by_ticker(ticker_symbol)
@@ -17,5 +17,9 @@ class Stock < ApplicationRecord
       rescue Exception => e
         return nil
       end
+    end
+
+    def self.find_by_ticker(ticker_symbol)
+      where(ticker: ticker_symbol).first
     end
 end
